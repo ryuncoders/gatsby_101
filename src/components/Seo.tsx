34c62d064c1200ce02951e50b0daa5c4b5cfe1de
ryuncoders <seungjy10@gmail.com>
2024-06-04 17:13:28 +0900

    feat(Seo): useStaticQuery

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,8 +1,22 @@
+import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
 interface ISeo {
   title: string;
 }
 
 export default function Seo({ title }: ISeo) {
-  return <title>{title} | DevStickers⭐</title>;
+  const data = useStaticQuery<Queries.seoDataQuery>(graphql`
+    query seoData {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  return (
+    <title>
+      {title} | {data.site?.siteMetadata?.title}⭐
+    </title>
+  );
 }
